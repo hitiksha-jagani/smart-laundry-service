976fc40f979393a7634da36d5b69e6e77a253ac7
Hitiksha Jagani <140380121+hitiksha-jagani@users.noreply.github.com>
2025-05-31 23:26:39 +0530

    Delete SmartLaundry1,0/SmartLaundry1-0 directory

--- SmartLaundry1,0/SmartLaundry1-0/src/main/java/com/SmartLaundry/exception/PasswordMisMatchException.java
+++
@@ -1,7 +0,0 @@
-package com.SmartLaundry.exception;
-
-public class PasswordMisMatchException extends RuntimeException{
-    public PasswordMisMatchException(){
-        super("Password and confirm password do not match.");
-    }
-}
--- SmartLaundry1,0/SmartLaundry1-0/src/main/java/com/SmartLaundry/exception/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.SmartLaundry.exception;
-
-public class UserAlreadyExistsException extends RuntimeException{
-    public UserAlreadyExistsException() {
-        super("User already exists.");
-    }
-}
