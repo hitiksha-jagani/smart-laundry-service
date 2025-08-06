75b711729aafa99fce5fccb39bffb79c33f6dc52
Bhavika Patel <nencypatel2212@gmail.com>
2025-08-06 12:31:10 +0530

    changes

---
+++ Backend/SmartLaundry1-0/src/test/java/com/SmartLaundry/Application.java
@@ -0,0 +1,23 @@
+//package com.SmartLaundry;
+//
+//import org.junit.jupiter.api.Test;
+//
+//class ApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//        System.out.println("Test is working!");
+//    }
+//}
+package com.SmartLaundry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
