30628a20b38a651fafec1e841db66e059103ffcb
Bhavika Patel <nencypatel2212@gmail.com>
2025-08-06 19:14:34 +0530

    changes

--- web_frontend/src/utils/axiosInstance.js
+++ web_frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+import { BASE_URL } from "./config";
 const instance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: BASE_URL,
 });
 
 // ✅ Public routes (no auth token required)
